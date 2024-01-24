e08ad159f5293d7e13a4dfad2fcb8f0d1dc1477f
naveen <naveenguttedar845@gmail.com>
2024-01-24 20:49:22 +0530

    added autocomplition

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -1,26 +1,31 @@
 return{
     {    "williamboman/mason.nvim",
-    config=function()
-        require("mason").setup()
-    end
-},
-{
-    "williamboman/mason-lspconfig.nvim",
-    config=function()
-        require("mason-lspconfig").setup {
-            ensure_installed = { "html","cssls","cssmodules_ls","emmet_ls","quick_lint_js","tsserver","vtsls","emmet_language_server"}
-    }
-end
+        config=function()
+            require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config=function()
+            require("mason-lspconfig").setup {
+                ensure_insalled = { "html","cssls","cssmodules_ls","emmet_ls","vtsls","emmet_language_server"}
+            }
+        end
 
-},
-{
-    "neovim/nvim-lspconfig",
-    config=function()
-        local lspconfig = require('lspconfig')
-        lspconfig.html.setup {}
-        lspconfig.vtsls.setup {}
-        lspconfig.cssls.setup {}
-        lspconfig.tsserver.setup {}
-    end
-}
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config=function()
+            local lspconfig = require('lspconfig')
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            lspconfig.html.setup {
+                capabilities=capabilities
+            }
+            lspconfig.vtsls.setup {
+
+                capabilities=capabilities
+            }
+            lspconfig.cssls.setup {capabilities=capabilities}
+        end
+    }
 }
